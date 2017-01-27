81fa2b1f39a046d9b993c88c27ffce43b73bccd6
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-01-27 17:38:11 +0300

    lab-05 complete

--- lab-05/src/main/java/edu/teldir/security/MyUserDetailsService.java
+++ lab-05/src/main/java/edu/teldir/security/MyUserDetailsService.java
@@ -1,15 +1,38 @@
 package edu.teldir.security;
 
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * @author Anton German &lt;AGerman@luxoft.com&gt;
  * @version 1.0 27.04.12
  */
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Setter
+    private String username;
+    @Setter
+    private String password;
+    @Setter
+    private Collection<String> roles;
+
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        if (username == null || !username.equals(this.username))
+            throw new UsernameNotFoundException("No user found: " + username);
+
+        Collection<GrantedAuthority> authorities = new ArrayList<>(roles.size());
+
+        for (String role : roles)
+            authorities.add(new SimpleGrantedAuthority(role));
+
+        return new User(this.username, password, authorities);
     }
 }
--- lab-05/src/main/resources/etc/users-info-tables.sql
+++ lab-05/src/main/resources/etc/users-info-tables.sql
@@ -17,7 +17,6 @@ CREATE TABLE authorities (
   username  VARCHAR(50) NOT NULL,
   authority VARCHAR(50) NOT NULL,
   FOREIGN KEY (username) REFERENCES users (username)
-  -- UNIQUE INDEX authorities_idx_1 (username, authority)
 );
-
+-- UNIQUE INDEX authorities_idx_1 (username, authority)
 CREATE UNIQUE INDEX ON authorities (username, authority)
